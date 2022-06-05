091ccd72d2b6c3f04556bf8fcc9c1804f0ada881
Adolfo Blanco <ajb156@outlook.com>
2022-06-05 09:47:49 +0200

    Login, Recuperar Contraseña

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,31 +1,93 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom";
+import { Alerta } from "../components/Alerta";
+import { clienteAxios } from "../helpers/axios";
 
 export const NuevoPassword = () => {
+  const [alerta, setAlerta] = useState({});
+  const [password, setPassword] = useState("");
+  const [tokenValido, setTokenValido] = useState(false);
+  const { token } = useParams();
+  const { msg } = alerta;
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        await clienteAxios.get(
+          `/usuarios/olvide-password/${token}`
+        );
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
+      }
+    };
+    comprobarToken();
+  }, []);
+
+  const handleSubmit =async(e) => {
+    e.preventDefault();
+
+    if (password === "" || password.length < 3) {
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      await clienteAxios.post(`/usuarios/olvide-password/${token}`, {
+        password,
+      });
+      setPassword("");
+      toast.success("Contraseña actualizada correctamente");
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-sky-600 font-black text-4xl capitalize text-center">
         Restablecer Contraseña
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
-        <div className="my-5">
-          <label htmlFor="password" className="input-label">
-            Nueva Contraseña
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Contraseña"
-            className="input-form"
-          />
-        </div>
+      {msg && <Alerta alerta={alerta} />}
 
-        <input
-          type="submit"
-          value="Confirmar Contraseña"
-          className="button-full"
-        />
-      </form>
+      {tokenValido && (
+        <form
+          className="my-10 bg-white shadow rounded-lg px-10 py-5"
+          onSubmit={handleSubmit}
+        >
+          <div className="my-5">
+            <label htmlFor="password" className="input-label">
+              Nueva Contraseña
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+              className="input-form"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Confirmar Contraseña"
+            className="button-full"
+          />
+        </form>
+      )}
     </Fragment>
   );
 };
--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,14 +1,47 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Alerta } from "../components/Alerta";
+import { clienteAxios } from "../helpers/axios";
 
 export const OlvidePassword = () => {
+  const [alerta, setAlerta] = useState({});
+  const { msg } = alerta;
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || email.length < 6) {
+      setAlerta({
+        msg: "El email es obligatorio",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post(`/usuarios/olvide-password`, {
+        email,
+      });
+      toast.success(data.msg);
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-sky-600 font-black text-5xl capitalize text-center">
         Recupera tu acceso
       </h1>
-
-      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white shadow rounded-lg px-10 py-5"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label htmlFor="email" className="input-label">
             Email
@@ -18,6 +51,9 @@ export const OlvidePassword = () => {
             type="email"
             placeholder="Email de registro"
             className="input-form"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
